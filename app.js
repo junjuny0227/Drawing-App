c2fbbe61e0056475b15e93485331260c7d6c2611
junjuny0227 <junjuny0227@gmail.com>
2024-08-28 21:09:44 +0900

    Changing Thickness

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
-
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 function onMove(event) {
   if (isPainting) {
@@ -22,7 +23,13 @@ function cancelPainting() {
   isPainting = false;
 }
 
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
